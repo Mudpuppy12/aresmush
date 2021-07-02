d085a0e7e0ae824961c18fe8b9db0eeacfb4b204
Dennis DeMarco <dennis@demarco.com>
2021-07-02 18:20:42 -0500

    foo

--- plugins/custom/mroll_cmd.rb
+++ plugins/custom/mroll_cmd.rb
@@ -1,17 +1,16 @@
 module AresMUSH
-    module Custom
-      class MrollCmd
-        include CommandHandler
-        
-        def parse_args
-         self.mroll = trim_arg(cmd.args)
-        end
-  
-        def handle
-          client.emit_success "Mroll executed"
-        end
+  module Custom
+    class MrollCmd
+      include CommandHandler
+      
+
+      def parse_args
+       self.goals = trim_arg(cmd.args)
+      end
+
+      def handle
+        client.emit_success "MROLL"
       end
     end
   end
 end
-  
