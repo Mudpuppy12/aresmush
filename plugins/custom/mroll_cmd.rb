2d474a13b8a5a51c4a9022e8481eb376dbb9c576
Dennis DeMarco <dennis@demarco.com>
2021-07-02 20:30:43 -0500

    re

--- plugins/custom/mroll_cmd.rb
+++ plugins/custom/mroll_cmd.rb
@@ -17,7 +17,7 @@ module AresMUSH
         # help
 
         client.emit_success "First Essence :" + self.first_essence
-        if self.second_essence 
+        if args.arg2 
           client.emit_success "Second Essence :" + self.second_essence
         end
         #if args.arg2
