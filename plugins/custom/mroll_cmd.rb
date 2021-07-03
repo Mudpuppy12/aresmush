f1590013746b2e7f532cf1a3eb4de13d79a25546
Dennis DeMarco <dennis@demarco.com>
2021-07-02 20:52:23 -0500

    dope

--- plugins/custom/mroll_cmd.rb
+++ plugins/custom/mroll_cmd.rb
@@ -10,14 +10,14 @@ module AresMUSH
 
         args = cmd.parse_args(ArgParser.arg1_slash_arg2)
         
-        self.first_essence = list_arg(args.arg1,"/").at(0)
-        self.first_num = list_arg(args.arg1,"/").at(1)
+        self.first_essence = list_arg(args.arg1,"=").at(0)
+        self.first_num = list_arg(args.arg1,"=").at(1)
 
         # help
 
         client.emit_success "First Essence :" + self.first_essence
         client.emit_success "First num :" + self.first_num.to_s
-        
+
        
         #if args.arg2
         #  second_essence = args.arg2.parse_args(/(?<essence>[\S]*)[=](?<num>[\d]+$)/)
