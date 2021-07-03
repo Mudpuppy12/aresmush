b7e5ea32c53a42b342e41ebc06dfd6701a36e938
Dennis DeMarco <dennis@demarco.com>
2021-07-02 21:19:33 -0500

    foo

--- plugins/custom/mroll_cmd.rb
+++ plugins/custom/mroll_cmd.rb
@@ -14,10 +14,7 @@ module AresMUSH
         
           self.first_essence = list_arg(args.arg1,"=").at(0)
           self.first_num = list_arg(args.arg1,"=").at(1)
-
-          client.emit_success "First Essence :" + self.first_essence
-          client.emit_success "First num :" + self.first_num.to_s
-
+          
 
         if args.arg2
           self.second_essence = list_arg(args.arg2,"=").at(0)
@@ -36,7 +33,9 @@ module AresMUSH
      end
       
       def handle
-        client.emit_success "MROLL"
+        client.emit_success "First Essence :" + self.first_essence
+        client.emit_success "First num :" + self.first_num.to_s
+
       end
     end
   end
