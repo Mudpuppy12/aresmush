cc8ec81096ce048e2621123abb63635033757f20
Dennis DeMarco <dennis@demarco.com>
2021-07-02 20:04:22 -0500

    no

--- plugins/custom/mroll_cmd.rb
+++ plugins/custom/mroll_cmd.rb
@@ -9,18 +9,6 @@ module AresMUSH
       def parse_args
 
         args = cmd.parse_args(ArgParser.arg1_slash_arg2)
-
-        client.emit_success "Ok"
-
-        first_essence = args.arg1.parse_args(/(?<essence>[\S]*)[=](?<num>[\d]+$)/)
-   
-        if args.arg2
-          second_essence = args.arg2.parse_args(/(?<essence>[\S]*)[=](?<num>[\d]+$)/)
-        end
-
-        self.first_num = args.arg1.num.to_i
-        self.first_essence = args.arg1.essence
-
       end
 
       def required_args
@@ -28,8 +16,7 @@ module AresMUSH
       end
       
       def handle
-        client.emit_success "MROLL=> " + self.first_essence + " Dice: " + self.first_num.to_s
-      end
+        client.emit_success "MROLL"
     end
   end
 end
